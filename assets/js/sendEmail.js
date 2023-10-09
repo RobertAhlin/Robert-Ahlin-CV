6f2f86c265ca33a983f6426146f5d82d838e7c26
RobertAhlin <>
2023-10-09 13:20:10 +0000

    Changed email conficration in contact form again.

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -7,9 +7,13 @@ function sendMail(contactForm) {
         .then(
             function (response) {
                 console.log("SUCCESS", response);
+                // Show a success message in a popup including from_name and from_email
+                alert(`Thank you ${contactForm.name.value}. Email has been sent successfully. I will reply as soon as possible to ${contactForm.emailaddress.value}!`);
             },
             function (error) {
                 console.log("FAILED", error);
+                // Show an error message in a popup
+                alert("Failed to send email. Please try again later.");
             }
         );
     return false;  // To block from loading a new page
