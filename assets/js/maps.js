0636f90da14d7b3d4250bd5d937d1a29248770be
RobertAhlin <>
2023-10-09 08:55:58 +0000

    updated google maps markers

---
+++ assets/js/maps.js
@@ -0,0 +1,26 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: {
+            lat: 57.800965,
+            lng: 12.360680
+        }
+    });
+    
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+        { lat: 57.800965, lng: 12.360680 }, 
+        { lat: 53.29893857800853, lng: -6.178931913078988 }
+        
+    ];
+
+    var markers = locations.map(function (location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
